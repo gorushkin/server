3826ef27ca4d67ba78abf440cc1360ec92ecbd06
gorushkin <gorushkin@gmail.com>
2021-08-31 23:02:20 +0300

    add bootstrap

--- server-frontend/src/index.jsx
+++ server-frontend/src/index.jsx
@@ -4,6 +4,7 @@ import { Provider } from 'react-redux';
 import App from './App';
 import './styles/styles.scss';
 import store from './store';
+import 'bootstrap/scss/bootstrap.scss';
 
 ReactDOM.render(
   <Provider store={store}>
